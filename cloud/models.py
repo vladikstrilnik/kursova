7746d85b873e7c6f52a99d139f7219a25336eb26
vladikstrilnik <vladikstrilnik555@gmail.com>
2026-07-02 16:31:47 +0300

    Update models.py

--- cloud/models.py
+++ cloud/models.py
@@ -1,1 +1,10 @@
+class WaterData:
 
+    def __init__(self,
+                 device,
+                 liters,
+                 flow):
+
+        self.device = device
+        self.liters = liters
+        self.flow = flow
